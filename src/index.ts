f10cfab485b487a4584cd2b42e5b12be66e7c889
Oleksandr <oleksandrshtonda>
2024-09-17 09:03:24 +0200

    init commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import {Client, Events, GatewayIntentBits} from "discord.js";
+import { token } from "../config";
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, readyClient => {
+  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.login(token);
